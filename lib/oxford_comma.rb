2fce57fd1f1b822e2bfcd9a15076988d00a0badc
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-15 02:49:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
+
+
+
+
+
+
 def oxford_comma(array)
+  if array.length == 1
+    return array [0]
+  elsif array.length == 2
+     array.join(" and ")
+  elsif array.length >= 3
+     last_array = array.pop
+     array.join(", ") + ", and " + last_array
+  end
 
 end
