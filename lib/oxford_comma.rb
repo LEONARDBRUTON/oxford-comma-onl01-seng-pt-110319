1e6b5bff90a551eec9c30aa442ccd7c17855985d
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-15 02:22:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,9 +8,9 @@ def oxford_comma(array)
   if array.length == 1
     return array [0]
   elsif array.length == 2
-    return array.join(" and ")
+     array.join(" and ")
   elsif array.length == 3
-    return array.join(", ")
+     array.join(", ")
   end
   array
 end
